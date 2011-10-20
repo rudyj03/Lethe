db8c549b1beb21e913b554621e4fb34dd755c549
rudyj03 <rudyj03@gmail.com>
2011-10-19 21:01:15 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lethe::Application.config.secret_token = 'ce5cd06da3008a856257f57be9354cb858bd1f235060cdb5ddef30490e5979836be503052cb990e26344a7c322d6ed85104e8f48dcfd89993707b33438302365'
