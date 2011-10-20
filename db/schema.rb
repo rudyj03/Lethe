32bb264af989a6a3223750b090e4950deef7e524
rudyj03 <rudyj03@gmail.com>
2011-10-20 00:41:36 -0400

    added password encryption for users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111020013138) do
+ActiveRecord::Schema.define(:version => 20111020043419) do
 
   create_table "items", :force => true do |t|
     t.integer  "owner"
@@ -26,6 +26,10 @@ ActiveRecord::Schema.define(:version => 20111020013138) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
+
+  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
 
 end
