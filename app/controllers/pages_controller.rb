a12ab37e1a3b5eb29b2e8a8ddff7dbc92dec0b0f
rudyj03 <rudyj03@gmail.com>
2011-10-26 01:03:36 -0400

    moved avatar and user name into the user info box

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,6 +2,7 @@ class PagesController < ApplicationController
   def index
     @title = "Home"
     @item = Item.new if signed_in?
+    redirect_to user_path(current_user) if signed_in?
   end
 
 end
