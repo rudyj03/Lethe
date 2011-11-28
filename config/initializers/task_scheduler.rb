a4190e8c1e7d68ca2138032b960791daf5cb65d8
rudyj03 <rudyj03@gmail.com>
2011-11-28 00:00:04 -0500

    removing daily tasks for now until I can place a limit on the number of overdue emails that have been sent to an individual

--- config/initializers/task_scheduler.rb
+++ config/initializers/task_scheduler.rb
@@ -2,6 +2,6 @@ scheduler = Rufus::Scheduler.start_new
 #scheduler.cron("0 12 * * *") do
 
 scheduler.cron("0 12 * * *") do
-  UserMailer.reminder_email.deliver
-  UserMailer.overdue_email.deliver
+  #UserMailer.reminder_email.deliver
+  #UserMailer.overdue_email.deliver
 end
